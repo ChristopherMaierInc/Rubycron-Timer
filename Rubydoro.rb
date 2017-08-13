3913f42de5c68849e6bcf941b59bdb2beed4af15
Christopher Maier <30882729+ChristopherMaierInc@users.noreply.github.com>
2017-08-13 18:33:39 +1000

    Add files via upload
    Graphics and notification sounds updated.
    Classes added.

--- Rubydoro.rb
+++ Rubydoro.rb
@@ -1,9 +1,11 @@
 #Prerequisite Ruby gems
-require 'ruby-progressbar'
 require 'colorize'
 require 'rainbow'
 require 'rainbow/ext/string'
 require 'os'
+require 'win32/sound'
+
+include Win32
 
 def clearscreen #Method to clear the screen, with command used dependent on OS running, detected through os gem
   if OS.mac? == true
@@ -11,6 +13,41 @@ def clearscreen #Method to clear the screen, with command used dependent on OS r
   else
     system "cls"
   end
+end
+
+class Sounds #All of the programs Sounds
+
+    def start
+      if OS.mac? == true
+        say 'Welcome to Rubydoro Timer'
+      else
+        Sound.play("startup.wav")
+      end
+    end
+
+    def bnotify
+      if OS.mac? == true
+        say 'Would you like to start your break?'
+      else
+        Sound.play("notify.wav")
+      end
+    end
+
+    def wnotify
+      if OS.mac? == true
+        say 'Would you like to start your next work block?'
+      else
+        Sound.play("notify.wav")
+      end
+    end
+
+    def error
+      if OS.mac? == true
+        say 'Program error'
+      else
+        Sound.play("error.wav")
+      end
+    end
 end
 
 class Graphics #All of the programs graphics
@@ -35,8 +72,8 @@ class Graphics #All of the programs graphics
       puts "                            ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝╚═╝  ╚═╝                "
       puts "                                                                                "
       puts "                ----------------------------------------------------------      "
-      puts "                                    A Ruby program by:                          "
-      puts "                           Christoper Maier & Filip Maslovaric                  "
+      puts "                                     A Ruby program by:                         "
+      puts "                            Christoper Maier & Filip Maslovaric                 "
       puts "                ----------------------------------------------------------      "
     end
 
@@ -66,10 +103,10 @@ class Graphics #All of the programs graphics
       puts "                       !...:!TVBBBRPFT||||||||||!!^^""'     ||||"
       puts "                       !.......:!?|||||!!^^""'              ||||"
       puts "                       !.........||||                     ||||"
+      puts "                       !.........||||                     ||||"
       puts "                       !.........||||      Rubydoro       ||||"
       puts "                       !.........||||                     ||||"
       puts "                       !.........||||        Timer        ||||"
-      puts "                       !.........||||                     ||||"
       puts "                       !.........||||                     ||||"
       puts "                       `.........||||                    ,||||"
       puts "                        .;.......||||               _.-!!|||||"
@@ -81,7 +118,7 @@ class Graphics #All of the programs graphics
       puts "                    !........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo."
       puts "                    `..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo."
       puts "                    `..........:::::::::::::::::::::::;iof688888888888b.     `YBBBP^'"
-      puts "                    `........::::::::::::::::;iof688888888888888888888b.     `"
+      puts "                    `........::::::::::::::::;iof688888888888888888888b.     "
       puts "                    `......:::::::::;iof688888888888888888888888888888b."
       puts "                      `....:::;iof688888888888888888888888888888888899fT!"
       puts "                        `..::!8888888888888888888888888888888899fT|!^''"
@@ -104,13 +141,6 @@ class Timer #Create a Timer class with methods for the timer component of Rubydo
     puts "#{word}" #Outputs text passed in as a parameter when the method is first called
     while Time.now < end_time #Check the local time every second until the present time and calculated end time are the same, at which point the timer stops.
       sleep 1
-        # 5.times {puts}
-        # array = Array.new(1001)
-        # progressbar = ProgressBar.create(:total => array.size)
-        # array.each do |item|
-        #   progressbar.increment
-        #   sleep 0.01
-        # end
     end
   end
 
@@ -118,12 +148,15 @@ time = Timer.new #Declare a new Timer object
 
 gfx = Graphics.new #Declare a new Graphics object
 
+sfx = Sounds.new #Decare a new Sounds object
+
 clearscreen
 
 gfx.space(5)
 gfx.title #Displays the title
 gfx.space(2)
-# puts "#{" " * 38}#{Rainbow("Rubydoro Timer").color("#FF6347")}"
+sfx.start
+
 puts "#{" " * 11}The #{Rainbow("Pomodoro").color("#FF6347")} technique is a workload managment lifehack developed by"
 puts "#{" " * 9}#{Rainbow("Francesco Cirillo").aqua} in the late 1980s to make your productivity #{Rainbow("SKYROCKET").lightskyblue}!"
 gfx.space(1)
@@ -140,19 +173,24 @@ input = gets.chomp.upcase
   if input == "YES" #Starts the timer component
     #begin
       work_thread = Thread.new do
-        #system(%Q{echo "\a"})
         time.track(10, "#{" " * 15}That's the spirit! Let's get our productivity caps on and work for 25 minutes straight!")
         clearscreen
         puts
         gfx.work
         gfx.space(3)
-        puts "#{" " * 15}Woohoo! You made it. We've definitely earned ourselves a 5 minute break.\n"
-        puts "#{" " * 34}Press #{Rainbow("return").color("#FF6347")} to start your break..."
+        # puts "Woohoo! You made it. We've definitely earned ourselves a 5 minute break.\n".center(100, " ")
+        # puts "Would you like to start your break?\n\n\n".center(100, " ")
+        # puts "Please enter (#{Rainbow("Y").color("#FF6347")})es to continue or (#{Rainbow("N").color("#FF6347")})o to exit.".center(120, " ")
+        puts "#{" " * 10}Woohoo! You made it. We've definitely earned ourselves a 5 minute break.\n\n"
+        puts "#{" " * 29}Would you like to start your break?\n\n\n\n"
+        puts "#{" " * 23}Please enter (#{Rainbow("Y").color("#FF6347")})es to continue or (#{Rainbow("N").color("#FF6347")})o to exit."
+        sfx.bnotify
         end
 
       gets
       clearscreen
       gfx.space(5)
+      sfx.error
       puts "#{" " * 45}#{Rainbow("Rubydoro terminated. Exiting...").color("#FF6347")}"
       sleep 1
       Thread.kill(work_thread)
@@ -165,13 +203,15 @@ input = gets.chomp.upcase
               gfx.space(5)
               gfx.coffee
               gfx.space(5)
-              puts "#{" " * 15}Ahhh, all refreshed now I hope? Time to get back into it!\n"
+              puts "#{" " * 15}Ahhh, all refreshed now I hope? Time to get back into it!\n\n"
               puts "#{" " * 16}Press #{Rainbow("return").color("#FF6347")} to start your next 25 minute work block..."
+              sfx.wnotify
         end
 
         gets
         clearscreen
         gfx.space(5)
+        sfx.error
         puts "#{" " * 45}#{Rainbow("Rubydoro terminated. Exiting...").color("#FF6347")}"
         Thread.kill(break_thread)
         # If program terminated, do NOT do a final 'clearscreen screen' to notify user.
@@ -180,17 +220,19 @@ input = gets.chomp.upcase
     clearscreen
     gfx.space(5)
     puts "#{" " * 30}No problem at all! Have an #{Rainbow("awesome").color("#FF6347")} day."
-
+    sfx.bnotify
   else #If the user doesn't enter either of the above, raises an error
     clearscreen
     gfx.space(5)
+    sfx.error
     puts "#{" " * 15}Something has gone #{Rainbow("horribly").color("#FF6347")} wrong.\n"
     raise "ERROR_WRONG_INPUT"
     sleep 5
   end
 
 rescue #Asks the user to enter either of the defined inputs above
-  puts "Please re-enter your answer as YES or NO only. I can wait. Indefinitely."
+  sfx.error
+  puts "#{" " * 30}Please re-enter your answer as #{Rainbow("YES").italic.bright.underline} or #{Rainbow("NO").italic.bright.underline} only. I can wait. Indefinitely."
   retry #Starts the loop again at the "Your Answer: " point
 end
 
